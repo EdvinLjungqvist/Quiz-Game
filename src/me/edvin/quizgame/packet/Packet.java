04aff43431b30f00631d08f122a39dadff8bfafb
Edvin <143885343+EdvinLjungqvist@users.noreply.github.com>
2023-11-02 16:28:36 +0100

    Update Packet.java

--- src/me/edvin/quizgame/packet/Packet.java
+++ src/me/edvin/quizgame/packet/Packet.java
@@ -7,14 +7,14 @@ import java.io.IOException;
 public abstract class Packet {
 
     /**
-     * Sends data to a output stream
+     * Sends data to an output stream
      * @param out output stream
      * @throws IOException if I/O error occurs
      */
     public abstract void encode(DataOutputStream out) throws IOException;
 
     /**
-     * Reads data from a input stream
+     * Reads data from an input stream
      * @param in input stream
      * @throws IOException if I/O error occurs
      */
